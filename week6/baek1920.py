3cf374685c0abfb781c7e1596041c01a64806e22
joo6881 <joo6881@gmail.com>
2023-02-13 12:46:23 +0900

    수 찾기

---
+++ week6/baek1920.py
@@ -0,0 +1,9 @@
+N = int(input())
+arr1 = set(list(map(int, input().split())))
+M = int(input())
+arr2 = list(map(int, input().split()))
+for i in arr2 :
+    if i in arr1:
+        print(1)
+    else :
+        print(0)
