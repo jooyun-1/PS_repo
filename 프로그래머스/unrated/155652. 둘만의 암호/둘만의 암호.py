18332f08ea88dada296bc7ac614b2959527d3026
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2023-08-10 09:43:07 +0900

    [unrated] Title: 둘만의 암호, Time: 0.20 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.py
@@ -0,0 +1,18 @@
+# s의 각 알파벳을 index만큼 뒤의 알파벳으로 바꾼다
+# index만큼의 뒤의 알파벳이 z를 넘어가면 a로 돌아감
+# skip에 있는 알파벳은 제외하고 건너뜀
+
+def solution(s, skip, index):
+    answer = ''
+    for c in s :
+        i = ord(c)
+        j = index
+        while j > 0 :
+            i += 1
+            if i > ord('z') :
+                i = ord('a')
+            if chr(i) in skip :
+                j += 1
+            j -= 1
+        answer += chr(i)
+    return answer
