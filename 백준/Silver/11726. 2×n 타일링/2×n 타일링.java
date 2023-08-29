79153a1b4a48af206182b49fa02868d8d6d9576c
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2023-08-29 16:29:19 +0900

    [Silver III] Title: 2×n 타일링, Time: 220 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] arr=new int[1001];
+		
+		arr[0]=0;
+		arr[1]=1;
+		arr[2]=2;
+		
+		for(int i=3;i<=n;i++) {
+			arr[i]=(arr[i-1]+arr[i-2])%10007;
+		}
+		System.out.println(arr[n]);
+	}
+}
