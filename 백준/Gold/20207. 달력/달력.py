b63bc3c6f54ff4bbb95cf5bbcfbf17e47f424419
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2023-08-30 09:58:36 +0900

    [Gold V] Title: 달력, Time: 128 ms, Memory: 115004 KB -BaekjoonHub

--- 백준/Gold/20207. 달력/달력.py
+++ 백준/Gold/20207. 달력/달력.py
@@ -1,20 +1,19 @@
-import sys
-
-N = int(sys.stdin.readline())
+N = int(input())
 arr = [0] * 366
-for i in range(N) :
-    start, end = map(int,sys.stdin.readline().split())
+
+for n in range(N) :
+    start, end = map(int,input().split())
     while start <= end :
         arr[start] += 1
         start += 1
 
 w, h = 0, 0
 answer = 0
-for i in range(366) :
+for i in range(1,366) :
     if arr[i] == 0 :
         answer += w*h
-        w, h = 0, 0
-        continue
-    w += 1
-    h = max(h, arr[i])
+        w,h = 0, 0
+    else :
+        w += 1
+        h = max(h,arr[i])
 print(answer + w*h)
