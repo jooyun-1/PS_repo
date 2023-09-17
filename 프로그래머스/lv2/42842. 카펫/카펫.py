736363e26d7c9eed6e01a4713fcff4599b90d4c1
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2023-09-17 20:54:05 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -0,0 +1,11 @@
+
+def solution(brown, yellow):
+    answer = []
+    sum = brown + yellow
+    for b in range(1,sum+1) :
+        if sum % b == 0 :
+            a = sum / b
+            if a >= b :
+                if 2*a + 2*b == brown + 4:
+                    answer = [a,b]
+    return answer
