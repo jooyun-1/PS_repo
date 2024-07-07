89f985f5d4255b8bb56f3ff717696ade3372c966
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2024-07-07 21:06:33 +0900

    [level 2] Title: 모음 사전, Time: 2.36 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.py
@@ -1,10 +1,18 @@
+# from itertools import permutations
 from itertools import product
 def solution(word):
-    words = []
     answer = 0
-    arr = ['A', 'E', 'I', 'O', 'U']
-    for i in range(1, 6):
-        for p in product(arr, repeat=i):
-            words.append(''.join(list(p)))
-    words.sort()
-    return words.index(word) + 1
+    dictionary = ['A','E','I','O','U']
+    arr = []
+    for i in range(1,6) :
+        # temp = list(permutations(dictionary,i))
+        temp = list(product(dictionary,repeat = i))
+        for perm in temp :
+            arr.append(''.join(perm))
+    arr.sort()
+    for a in arr :
+        if a == word :
+            answer += 1
+            return answer
+        answer += 1
+    return answer
