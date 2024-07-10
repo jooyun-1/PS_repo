f6dd40a269c5837e7ce4db19373fee365347d40f
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2024-07-10 21:25:00 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -2,8 +2,10 @@ def solution(progresses, speeds):
     answer = []
     time = 0
     cnt = 0
-    while len(progresses) > 0 :
-        if progresses[0] + (time * speeds[0]) >= 100 :
+    while True :
+        if len(progresses) < 1 :
+            break
+        if progresses[0] + time * speeds[0] >= 100 :
             progresses.pop(0)
             speeds.pop(0)
             cnt += 1
@@ -12,5 +14,5 @@ def solution(progresses, speeds):
                 answer.append(cnt)
                 cnt = 0
             time += 1
-    answer.append(cnt)         
+    answer.append(cnt)
     return answer
