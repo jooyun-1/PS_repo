351b6edde9fb3d472749aa636228925e96f56dcf
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2024-07-12 19:27:17 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.63 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,31 +1,34 @@
-dx = [1,-1,0,0]
-dy = [0,0,1,-1]
+from collections import deque
 
-
-def bfs(x,y,maps,n,m) :
-    
-    que = []
-    que.append([x,y])
-    visited = [[0] * m for _ in range(n)]
-    visited[x][y] = 1
-
-    while que :
-        x, y = que.pop(0)
-        if x == n-1 and y == m-1 :
-
-            return visited[x][y]
-        for i in range(4) :
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m :
-                if maps[nx][ny] == 1 and visited[nx][ny] == 0 :
-                    visited[nx][ny] = visited[x][y] + 1
-                    que.append([nx,ny])
 def solution(maps):
     answer = 0
     n = len(maps)
-    m = len(maps[len(maps)-1])
-    answer = bfs(0,0,maps,n,m)
-    if answer == None :
-        answer = -1
+    m = len(maps[0])
+    visited = [[0] * m for _ in range(n)]
+    deq = deque()
+    dx = [1,-1,0,0]
+    dy = [0,0,1,-1]
+    
+    def bfs(x, y) :
+        visited[x-1][y-1] = 1
+        deq.append((x-1,y-1))
+        flag = False
+        cnt = 1
+        while deq :
+            cx, cy = deq.popleft()
+            if cx == n -1 and cy == m - 1 :
+                flag = True
+                return visited[cx][cy]
+            for i in range(4) :
+                nx = cx + dx[i]
+                ny = cy + dy[i]
+                if 0 <= nx < n and 0 <= ny < m :
+                    if maps[nx][ny] == 1 and visited[nx][ny] == 0 :
+                        deq.append((nx,ny))
+                        visited[nx][ny] = visited[cx][cy] + 1
+                        cnt += 1
+        if flag == False :
+            return -1
+        
+    answer = bfs(1,1)
     return answer
