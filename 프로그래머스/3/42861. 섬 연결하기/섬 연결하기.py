7af0fdacf33468d0539331404c914463d18d6089
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2024-07-28 20:39:54 +0900

    [level 3] Title: 섬 연결하기, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.py
@@ -1,16 +1,15 @@
-# Kruskal
 def solution(n, costs):
     answer = 0
     costs.sort(key = lambda x : x[2])
     link = set([costs[0][0]])
     
     while len(link) != n :
-        for c in costs :
-            if c[0] in link and c[1] in link :
+        for v in costs :
+            if v[0] in link and v[1] in link :
                 continue
-            if c[0] in link or c[1] in link :
-                link.update([c[0], c[1]])
-                answer += c[2]
+            elif v[0] in link or v[1] in link :
+                answer += v[2]
+                link.update([v[0],v[1]])
                 break
-                
+        
     return answer
