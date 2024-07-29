d520ad6c20e9f92af578f034da5eeb0526d83537
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2024-07-29 21:11:14 +0900

    [level 3] Title: 입국심사, Time: 719.37 ms, Memory: 14.3 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -5,14 +5,11 @@ def solution(n, times):
         mid = (left + right) // 2
         people = 0
         for time in times :
-            # people 은 모든 심사관들이 mid분 동안 심사한 사람의 수
             people += mid // time
-            # # 모든 심사관을 거치지 않아도 mid분 동안 n명 이상의 심사를 할 수 있다면 반복문을 나간다.
-            # if people >= n:
-            #     break 
         if people >= n :
             answer = mid
             right = mid - 1
         elif people < n :
             left = mid + 1
+    
     return answer
