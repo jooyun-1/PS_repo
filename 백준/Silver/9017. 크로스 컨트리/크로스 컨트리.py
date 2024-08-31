fb5591842d44a552746e71390cacf10d7e8c90f8
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2024-08-31 23:22:50 +0900

    [Silver III] Title: 크로스 컨트리, Time: 124 ms, Memory: 114204 KB -BaekjoonHub

--- 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.py
+++ 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.py
@@ -1,21 +1,20 @@
 import sys
 from collections import Counter
-input = sys.stdin.readline
 
-T = int(input())
-for _ in range(T):
-  N = int(input())
-  teams = list(map(int, input().split()))
-  counter = Counter(teams)
-  scores = {}
-  rank = 1
-  for i in range(N):
-    if counter[teams[i]] == 6:
-      if teams[i] in scores:
-        scores[teams[i]].append(rank)
-      else:
-        scores[teams[i]] = [rank]
-      rank += 1
-  
-  print(sorted(scores, key = lambda x:(sum(scores[x][0:4]), scores[x][4]))[0])
-  
+t = int(sys.stdin.readline())
+count = {}
+
+for i in range(t) :
+    n = int(sys.stdin.readline())
+    teams = list(map(int,sys.stdin.readline().split()))
+    counter = Counter(teams)
+    scores = {}
+    rank = 1
+    for i in range(n) :
+        if counter[teams[i]] == 6 :
+            if teams[i] in scores :
+                scores[teams[i]].append(rank)
+            else :
+                scores[teams[i]] = [rank]
+            rank += 1
+    print(sorted(scores, key = lambda x:(sum(scores[x][0:4]), scores[x][4]))[0])
