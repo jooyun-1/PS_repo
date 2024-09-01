58e8fe33b822bcfb39fd407da50af046dc664956
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2024-09-01 22:56:01 +0900

    [Silver II] Title: 예산, Time: 96 ms, Memory: 110620 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -1,17 +1,19 @@
 import sys
-n = int(sys.stdin.readline()) # 지방의 수
-moneys = list(map(int,sys.stdin.readline().split()))
+
+n = int(sys.stdin.readline())
+
+arr = list(map(int,sys.stdin.readline().split()))
 m = int(sys.stdin.readline())
 answer = 0
-left, right = 0, max(moneys)
+left, right = 0, max(arr)
 while left <= right :
     mid = (left + right) // 2
     total = 0
-    for i in range(n) :
-        if moneys[i] > mid :
-            total += mid
+    for i in range(len(arr)) :
+        if mid >= arr[i] :
+            total += arr[i]
         else :
-            total += moneys[i]
+            total += mid
     if total <= m :
         answer = mid
         left = mid + 1
