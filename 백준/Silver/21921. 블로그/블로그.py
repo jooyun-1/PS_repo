974f9a19ffa0d910a8e01e817d867c2fba543904
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2024-09-02 22:04:47 +0900

    [Silver III] Title: 블로그, Time: 136 ms, Memory: 155520 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.py
+++ 백준/Silver/21921. 블로그/블로그.py
@@ -1,19 +1,18 @@
 import sys
 
-N, X = map(int,sys.stdin.readline().split())
-visited = list(map(int,sys.stdin.readline().split()))
-
-if max(visited) == 0 :
+n, x = map(int,sys.stdin.readline().split())
+arr = list(map(int,sys.stdin.readline().split()))
+if max(arr) == 0 :
     print('SAD')
     exit(0)
     
-val = sum(visited[:X])
+val = sum(arr[:x])
 max_val = val
 max_cnt = 1
 
-for i in range(X,N) :
-    val += visited[i]
-    val -= visited[i-X]
+for i in range(x,n) :
+    val += arr[i]
+    val -= arr[i-x]
     if val > max_val :
         max_val = val
         max_cnt = 1
