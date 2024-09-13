cf15fc2aa5ee2d3324a45b9cde3cca428a5cc2d7
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2024-09-13 21:37:33 +0900

    [Silver V] Title: 다리 놓기, Time: 88 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,13 +1,8 @@
-T = int(input())
-def combi_to_dp(r,c) :
-    if arr[r][c] != 0 :
-        return arr[r][c]
-    if r == c or c == 0 :
-        return 1
-    else :
-        arr[r][c] = combi_to_dp(r-1,c-1) + combi_to_dp(r-1,c)
-        return arr[r][c]
-for t in range(T) :
-    N, M = map(int,input().split())
-    arr = [[0] * (N+1) for _ in range(M+1)]
-    print(combi_to_dp(M,N))
+import sys
+import math
+
+T = int(sys.stdin.readline())
+
+for i in range(T) :
+    n, m = map(int,sys.stdin.readline().split())
+    print(math.comb(m,n))
