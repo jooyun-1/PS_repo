b0950ea969616c7ff7c8a027dd64e13de61f2f00
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2024-09-21 23:22:51 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 152 ms, Memory: 112352 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,30 +1,28 @@
-# 덧, 뺄, 곱, 나 순서 (입력)
-from itertools import permutations
 N = int(input())
-nums = list(map(int,input().rstrip().split()))
-line = list(map(int,input().rstrip().split()))
-operators = ['+', '-', '*', '//']
-arr = []
-for i in range(len(line)) :
-    for j in range(line[i]) :
-        arr.append(operators[i])
-permu = list(permutations(arr,len(arr)))
-max_sum, min_sum = float('-inf'), float('inf')
-for per in permu :
-    sum = nums[0]
-    for i in range(len(per)) :
-        if per[i] == '+' :
-            sum += nums[i+1]
-        elif per[i] == '-' :
-            sum -= nums[i+1]
-        elif per[i] == '//' :
-            if sum < 0 :
-                sum = (abs(sum) // nums[i+1]) * -1
-            else :
-                sum = sum // nums[i+1]
-        elif per[i] == '*' :
-            sum = sum * nums[i + 1]
-    max_sum = max(sum,max_sum)
-    min_sum = min(sum,min_sum)
-print(max_sum)
-print(min_sum)
+num = list(map(int, input().split()))
+op = list(map(int, input().split()))  # +, -, *, //
+
+maximum = -1e9
+minimum = 1e9
+
+
+def dfs(depth, total, plus, minus, multiply, divide):
+    global maximum, minimum
+    if depth == N:
+        maximum = max(total, maximum)
+        minimum = min(total, minimum)
+        return
+
+    if plus:
+        dfs(depth + 1, total + num[depth], plus - 1, minus, multiply, divide)
+    if minus:
+        dfs(depth + 1, total - num[depth], plus, minus - 1, multiply, divide)
+    if multiply:
+        dfs(depth + 1, total * num[depth], plus, minus, multiply - 1, divide)
+    if divide:
+        dfs(depth + 1, int(total / num[depth]), plus, minus, multiply, divide - 1)
+
+
+dfs(1, num[0], op[0], op[1], op[2], op[3])
+print(maximum)
+print(minimum)
