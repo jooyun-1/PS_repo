443ec451621165a9331e539f9ca35d8c9faf97e6
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2024-09-27 23:31:13 +0900

    [Gold IV] Title: 공유기 설치, Time: 184 ms, Memory: 118816 KB -BaekjoonHub

--- 백준/Gold/2110. 공유기 설치/공유기 설치.py
+++ 백준/Gold/2110. 공유기 설치/공유기 설치.py
@@ -1,34 +1,22 @@
-n, c = map(int, input().split())
-
-array = []
-for i in range(n):
-    array.append(int(input()))
-
-array.sort()
-
-
-def binary_search(array, start, end):
-    while start <= end:
-        mid = (start + end) // 2
-        current = array[0]
-        count = 1
-
-        for i in range(1, len(array)):
-            if array[i] >= current + mid:
-                count += 1
-                current = array[i]
-
-        if count >= c:
-            global answer
-            start = mid + 1
-            answer = mid
-        else:
-            end = mid - 1
-
-
-start = 1
-end = array[-1] - array[0]
+N, C = map(int,input().split())
+arr = []
+for i in range(N) :
+    x = int(input())
+    arr.append(x)
+arr.sort()
+start, end = 1, arr[-1] - arr[0]
 answer = 0
 
-binary_search(array, start, end)
+while start <= end :
+    mid = (start + end) // 2
+    cnt, cur = 1, arr[0]
+    for i in range(1,len(arr)) :
+        if arr[i] >= cur + mid :
+            cnt += 1
+            cur = arr[i]
+    if cnt >= C :
+        start = mid + 1
+        answer = mid
+    else :
+        end = mid - 1
 print(answer)
