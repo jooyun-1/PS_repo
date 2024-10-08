cacba9d11122da12527162be00f7c5ee1d869d81
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2024-10-08 15:11:46 +0900

    [Gold V] Title: 두 용액, Time: 120 ms, Memory: 126352 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,23 @@
+N = int(input())
+arr = list(map(int,input().split()))
+arr.sort()
+
+answer = []
+left, right = 0, N-1
+res = abs(arr[left] + arr[right])
+final = [arr[left], arr[right]]
+
+while left < right :
+    temp = arr[left] + arr[right]
+    if abs(temp) < res :
+        res = abs(temp)
+        final = [arr[left], arr[right]]
+        if res == 0 :
+            break
+
+    if temp < 0 :
+        left += 1
+    else :
+        right -= 1
+
+print(final[0], final[1])
