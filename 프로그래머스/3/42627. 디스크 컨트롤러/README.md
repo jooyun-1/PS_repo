95f92b79ba6ffba1aaaea62bfcbe93d94cc53c4a
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2024-10-14 21:43:13 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 17.51 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,10 +1,10 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 10.18 ms
+메모리: 10.2 MB, 시간: 17.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 21일 16:10:09
+2024년 10월 14일 21:43:10
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,28 +1,22 @@
 import heapq
-import math
 
 def solution(jobs):
     answer = 0
-    now_time = 0
-    finish = 0 # 완료한 작업
+    finish = 0
+    before = -1
     heap = []
-    total_time = 0
-    before_request = -1
-    
-    while finish < len(jobs):
-        # 현재 시간 기준 작업할 수 있는 애들 가져오기
-        for job in jobs:
-            if before_request < job[0] <= now_time:
-                heapq.heappush(heap, [job[1],job[0]]) # 위치 바꿔 넣기
-        # 작업        
-        if len(heap)>0: # 수행할 작업이 있다면
-            now_working = heapq.heappop(heap) # 소요시간이 가장 작은 애 꺼내기
-            before_request = now_time
-            now_time += now_working[0] # 일 끝난 시점으로 워프
-            total_time += (now_time - now_working[1]) # 이 작업이 대기하고 끝날때까지 걸린 시간
+    now = 0
+    while finish < len(jobs) :
+        for job in jobs :
+            if before < job[0] <= now :
+                heapq.heappush(heap,(job[1],job[0]))
+        if len(heap) > 0 :
+            now_working = heapq.heappop(heap)
+            before = now
+            now += now_working[0]
+            answer += (now - now_working[1])
             finish += 1
-            
-        else: # 수행할 작업이 없으면
-            now_time += 1
-    answer = int(total_time / len(jobs))
+        else :
+            now += 1
+    answer = int(answer/len(jobs))
     return answer
