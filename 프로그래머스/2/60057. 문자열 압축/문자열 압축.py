a0be7abb8473098e7433cfa1bd4a136e882b54e5
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2024-10-16 16:52:52 +0900

    [level 2] Title: 문자열 압축, Time: 5.78 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,24 +1,27 @@
 def solution(s):
-    answer = []
-    if len(s) == 1:
+    result=[]
+    
+    if len(s)==1:
         return 1
-    for i in range(1,len(s)//2+1) :
-        result = ''
-        temp = s[:i]
+    
+    for i in range(1, len(s)+1):
+        b = ''
         cnt = 1
-        for j in range(i,len(s),i) :
-            if temp == s[j:j+i] :
-                cnt += 1
-            else :
-                if cnt > 1 :
-                    result += str(cnt) + temp
-                else :
-                    result += temp
-                temp = s[j:j+i]
+        tmp=s[:i]
+
+        for j in range(i, len(s)+i, i):
+            if tmp==s[j:i+j]:
+                cnt+=1
+            else:
+                if cnt!=1:
+                    b += str(cnt)+tmp
+                else:
+                    b += tmp
+                    
+                tmp=s[j:j+i]
                 cnt = 1
-        if cnt > 1 :
-            result += str(cnt) + temp
-        else :
-            result += temp
-        answer.append(len(result))
-    return min(answer)
+                
+        result.append(len(b))
+        
+
+    return min(result)
