30b6722cc974c19dc613e65a4e0ec9733c6ad0ea
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2024-10-22 22:04:58 +0900

    [level 2] Title: 타겟 넘버, Time: 747.25 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,19 +1,14 @@
 def solution(numbers, target):
     global answer
     answer = 0
-    cnt = 0
-    total = 0
     arr = [1,-1]
-    temp = []
     def dfs(num, cnt) :
         global answer
-        if cnt == len(numbers):
+        if cnt == len(numbers) :
             if num == target :
                 answer += 1
-            return
-        for i in range(2) :
+            return 
+        for i in range(len(arr)) :
             dfs(num + arr[i] * numbers[cnt], cnt + 1)
-
-    dfs(0, 0)
-    
+    dfs(0,0)
     return answer
