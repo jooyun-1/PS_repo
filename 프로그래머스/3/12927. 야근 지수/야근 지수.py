fe41efafb8d433f7acc156876d726dacf1a0645f
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2024-10-30 17:55:30 +0900

    [level 3] Title: 야근 지수, Time: 478.28 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.py
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.py
@@ -1,17 +1,13 @@
-# 시작한 시점 += 남은 작업량 ** 2
-# 1시간의 작업량 1
 import heapq
-
 def solution(n, works):
-    if sum(works) <= n:
-        return 0
     answer = 0
+    if n >= sum(works):
+        return 0
+    
     works = [-w for w in works]
     heapq.heapify(works)
-    while n > 0:
-        max_val = heapq.heappop(works)
-        heapq.heappush(works, max_val+1)
-        n -= 1
-    for w in works:
-        answer += w ** 2
-    return answer
+    for _ in range(n):
+        i = heapq.heappop(works)
+        i += 1
+        heapq.heappush(works, i)
+    return sum([w ** 2 for w in works])
