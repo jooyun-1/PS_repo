36a8975399e40e5724147371c8e0bffce251ad89
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2024-10-31 11:49:21 +0900

    [level 3] Title: 단어 변환, Time: 6.63 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,28 +1,31 @@
+from collections import deque
+
 def solution(begin, target, words):
-    global answer, flag
-    answer = 51
-    visited = [0] * len(words)
-    flag = False
-    def dfs(s, result) :
-        global answer,flag
-        # print(s,visited)
-        if s == target :
-            flag = True
-            answer = min(answer,result)
-            # return result
-        for i in range(len(words)) :
-            # print(words[i])
-            cnt = 0
-            for j in range(len(begin)) :
-                if s[j] != words[i][j] :
-                    cnt += 1
-                if cnt > 1 :
-                    break
-            if cnt == 1 and visited[i] == 0:
-                visited[i] = 1
-                dfs(words[i], result + 1)
-                visited[i] = 0            
-    dfs(begin,0)
-    if flag == False :
-        return 0
-    return answer
+    
+    if target not in words : 
+        return  0
+    
+    return bfs(begin, target, words)
+
+
+#최소 단계를 찾아야 하므로 bfs
+def bfs(begin, target, words):
+
+    queue = deque()
+    queue.append([begin, 0]) #시작 단어와 단계 0으로 초기화
+    
+    while queue:
+        now, step = queue.popleft()
+        
+        if now == target:
+            return step
+        
+        #단어를 모두 체크하면서, 해당 단어가 변경될 수 있는지 체크
+        for word in words:
+            count = 0
+            for i in range(len(now)): #단어의 길이만큼 반복하여
+                if now[i] != word[i]: #단어에 알파벳 한개씩 체크하기
+                    count += 1
+                    
+            if count == 1: 
+                queue.append([word, step+1])
