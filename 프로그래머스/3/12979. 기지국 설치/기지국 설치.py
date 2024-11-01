4c55c6a7be6852c94ddaf3096556cda512b20d2e
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2024-11-01 19:35:40 +0900

    [level 3] Title: 기지국 설치, Time: 1.99 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
@@ -1,14 +1,14 @@
 import math
-
 def solution(n, stations, w):
     answer = 0
-    range = w + w + 1
     now = 1
-    for station in stations :
-        length = station - w - now
+    range = w + w + 1
+    for s in stations :
+        length = s - w - now
         if length > 0 :
-            answer += math.ceil(length / range)
-        now = station + w + 1
+            answer += math.ceil(length/range)
+        now = s + w + 1
     if n >= now :
-        answer += math.ceil((n - now + 1) / range)
+        answer += math.ceil((n-now+1)/range)
+    
     return answer
