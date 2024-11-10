8871cee2f02ad965e7e3e51338e8e1b954ee07f3
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2024-11-11 00:15:12 +0900

    [level 3] Title: 가장 먼 노드, Time: 8396.53 ms, Memory: 20 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,25 +1,22 @@
 from collections import deque
-
 def solution(n, edge):
     answer = 0
-    graph =[[] for _ in range(n+1)]
-    visited = [-1] * (n+1)
-    for e in edge :
-        graph[e[0]].append(e[1])
-        graph[e[1]].append(e[0])
+    graph = [[] for _ in range(n+1)]
+    distance = [-1] * (n+1)
     
-    deq = deque()
-    def bfs(node) :
-        deq.append(node)
-        visited[1] = 0
-        while deq :
-            x = deq.popleft()
-            for i in graph[x] :
-                if visited[i] == -1 :
-                    visited[i] = visited[x] + 1
-                    deq.append(i)
-    bfs(1)
-    for v in visited :
-        if v == max(visited) :
+    for ed in edge :
+        graph[ed[0]].append(ed[1])
+        graph[ed[1]].append(ed[0])
+    deq = deque([1])
+    distance[1] = 0
+    while deq :
+        now = deq.popleft()
+        for i in graph[now] :
+            if distance[i] == -1 :
+                deq.append(i)
+                distance[i] = distance[now] + 1
+    for d in distance :
+        if d == max(distance) :
             answer += 1
+            
     return answer
