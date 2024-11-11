30cc8f4efd81717cb0cd46614f9ac0df3ef3449d
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2024-11-11 19:57:17 +0900

    [level 3] Title: 입국심사, Time: 687.53 ms, Memory: 14.2 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,15 +1,15 @@
 def solution(n, times):
     answer = 0
-    left, right = 1, max(times) * n
-    while left <= right :
-        mid = (left + right) // 2
+    end = max(times) * n
+    start = 1
+    while start <= end :
+        mid = (start + end) // 2
         people = 0
         for time in times :
             people += mid // time
         if people >= n :
             answer = mid
-            right = mid - 1
+            end = mid - 1
         elif people < n :
-            left = mid + 1
-    
+            start = mid + 1
     return answer
