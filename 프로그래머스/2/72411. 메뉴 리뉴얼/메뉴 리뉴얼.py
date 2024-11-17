f0dd74d90d1226716d5a9f9319c0851fa7d9c044
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2024-11-17 21:01:22 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 96.34 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.py
@@ -1,22 +1,32 @@
 from itertools import combinations
 
 def solution(orders, course):
-    set_menu = dict()
     answer = []
-    for c in course :
-        for ord in orders :
-            for combi in combinations(ord,c) :
-                combi = sorted(combi)
-                combi = ''.join(combi)
-                if combi not in set_menu :
-                    set_menu[combi] = 0
-                set_menu[combi] += 1
-    set_menu = {foods : cnt for foods,cnt in set_menu.items() if cnt > 1}
-    set_menu = sorted(set_menu.items(), key = lambda x : (len(x[0]), -x[1]))
-    menu_cnt = dict(zip(course,[-1] * len(course)))
-    for food, cnt in set_menu :
-        if menu_cnt[len(food)] <= cnt :
-            menu_cnt[len(food)] = cnt
-            answer.append(food)
+    
+    
+    for course_item in course:
+        comb_count = {}
+        order_combinations = []
         
-    return sorted(answer)
+        max_order = []
+        for order in orders:
+            order_comb = combinations(list(order), course_item)
+            
+            for order in order_comb:
+                order_comb_string = "".join(sorted(order))
+                
+                if comb_count.get(order_comb_string):
+                    comb_count[order_comb_string] += 1
+                    
+                else:
+                    comb_count[order_comb_string] = 1
+            
+        max_order = [k for k,v in comb_count.items() if ((v == max(comb_count.values())) and v >= 2) ]
+        
+        answer.extend(max_order)
+        
+    
+    answer = sorted(answer)
+    
+    
+    return answer
