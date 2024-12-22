9bc1d97d59cfd133c2c76aa1dce6dbe4a3a1e8a0
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2024-12-23 00:53:16 +0900

    [level 4] Title: 징검다리, Time: 145.22 ms, Memory: 12.2 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.py
+++ 프로그래머스/4/43236. 징검다리/징검다리.py
@@ -1,30 +1,25 @@
 def solution(distance, rocks, n):
     answer = 0
-    
+    rocks.sort()
     rocks.append(distance)
-    rocks = sorted(rocks)
-    
-    left, right = 0, distance  
-    
-    while left <= right:
-        
-        mid = (left + right) // 2  
-        min_distance = float('inf')  
-        current = 0  
-        remove_cnt = 0  
-        
-        for rock in rocks:
-            diff = rock - current  
-            if diff < mid:  
-                remove_cnt += 1
-            else:  
-                current = rock  
-                min_distance = min(min_distance, diff)  
-        
-        if remove_cnt > n:  
+    left, right = 1, distance
+    delete = 0
+    prev_rock = 0
+    while left <= right :
+        mid = (left + right) // 2
+        delete = 0
+        prev_rock = 0
+        for rock in rocks :
+            diff = rock - prev_rock
+            if diff < mid :
+                delete += 1
+                if delete > n :
+                    break
+            else :
+                prev_rock = rock
+        if delete > n :
             right = mid - 1
-        else:  
-            answer = min_distance
+        else :
+            answer = mid
             left = mid + 1
-
     return answer
