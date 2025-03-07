ffd9e7f64538d3e9341dd7090e140123bec6e3e1
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2025-03-07 22:49:00 +0900

    [Silver II] Title: N과 M (12), Time: 112 ms, Memory: 110900 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,12 @@
+n, m = map(int, input().split())
+k = sorted(set(list(map(int, input().split()))))
+ans = []
+def solve(depth, idx) :
+    if depth == m :
+        print(' '.join(map(str, ans)))
+        return
+    for i in range(idx,len(k)) :
+        ans.append(k[i])
+        solve(depth+1,i)
+        ans.pop()
+solve(0,0)
