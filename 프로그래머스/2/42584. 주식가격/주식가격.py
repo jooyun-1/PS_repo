3f2922ff9eec25f4d793b9278576354afde743c7
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2025-04-21 22:15:46 +0900

    [level 2] Title: 주식가격, Time: 146.21 ms, Memory: 18.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -2,7 +2,7 @@ def solution(prices):
     answer = []
     for i in range(len(prices)-1) :
         cnt = 0
-        for j in range(i+1,len(prices)) :
+        for j in range(i+1, len(prices)) :
             if prices[i] <= prices[j] :
                 cnt += 1
             else :
