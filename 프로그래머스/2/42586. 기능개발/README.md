faaa18e488af26e022349e9fe74fb5cb790d6a5f
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2025-04-21 19:06:25 +0900

    [level 2] Title: 기능개발, Time: 0.60 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.01 ms
+메모리: 9.22 MB, 시간: 0.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 10일 21:24:57
+2025년 04월 21일 19:06:25
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,18 +1,26 @@
+# 각 기능 : 진도 100% -> 서비스 반영
+# 기능 개발 속도 다름
+# 앞에 있는 기능 배포될 때 함께 배포 (뒤에 있는 기능이 먼저 개발되면)
+
 def solution(progresses, speeds):
     answer = []
-    time = 0
-    cnt = 0
+    index = 0
+
     while True :
-        if len(progresses) < 1 :
+        cnt = 0
+        if len(progresses) == sum(answer) :
             break
-        if progresses[0] + time * speeds[0] >= 100 :
-            progresses.pop(0)
-            speeds.pop(0)
+        for i in range(len(progresses)) :
+            progresses[i] += speeds[i]
+        if progresses[index] >= 100 :
             cnt += 1
-        else :
-            if cnt > 0 :
-                answer.append(cnt)
-                cnt = 0
-            time += 1
-    answer.append(cnt)
+            for j in range(index,len(progresses)) :
+                if progresses[j] >= 100 :
+                    cnt += 1
+                else :
+                    break
+        if cnt > 0 :
+            answer.append(cnt-1)
+            index += cnt - 1
+
     return answer
