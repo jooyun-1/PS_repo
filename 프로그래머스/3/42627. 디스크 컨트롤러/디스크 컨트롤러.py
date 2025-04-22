370e5fc0931f5ccf76456f6d58ef4923caff3d57
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2025-04-22 22:11:58 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 19.67 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,23 +1,22 @@
+# 작업의 소요시간이 짧은 것, 작업의 요청 시각이 빠른 것, 작업의 번호가 작은 것
 import heapq
 
 def solution(jobs):
     answer = 0
-    finish = 0
     before = -1
-    now = 0
+    now, finish = 0, 0
     heap = []
+    
     while finish < len(jobs) :
         for job in jobs :
-            if before < job[0] <= now : 
-                heapq.heappush(heap, (job[1],job[0]))
+            if before < job[0] <= now :
+                heapq.heappush(heap,(job[1],job[0]))
         if len(heap) > 0 :
             time, req = heapq.heappop(heap)
             before = now
             now += time
-            answer += (now - req)
+            answer += (now-req)
             finish += 1
         else :
             now += 1
-    
-
     return answer // len(jobs)
