cd66f550d2089c75a65277e4b0a67e1e566215a4
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2025-04-24 00:42:54 +0900

    [level 2] Title: 구명보트, Time: 16.34 ms, Memory: 9.94 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -4,19 +4,15 @@ def solution(people, limit):
     answer = 0
     people.sort()
     people = deque(people)
-
-    while True :
+    while people :
         if len(people) > 1 :
             if people[0] + people[-1] <= limit :
                 people.popleft()
                 people.pop()
-                answer += 1
-            else :
+            elif people[0] + people[-1] > limit :
                 people.pop()
-                answer += 1                
-        elif len(people) == 1:
+        elif len(people) == 1 :
             people.pop()
-            answer += 1
-        else :
-            break
+        answer += 1
+    
     return answer
