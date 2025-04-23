a5c6721400806e47de8888ee1f0e19a05b6cddfa
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2025-04-23 23:49:31 +0900

    [level 2] Title: 큰 수 만들기, Time: 86.70 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,10 +1,16 @@
 def solution(number, k):
     answer = []
-    for n in number :
-        while k > 0 and answer and answer[-1] < n :
-            answer.pop()
-            k -= 1
-        answer.append(n)
+    for num in number :
+        if not answer :
+            answer.append(num)
+            continue
+        if k > 0 :
+            while answer[-1] < num :
+                answer.pop()
+                k -= 1
+                if not answer or k <= 0 :
+                    break
+        answer.append(num)
     if k > 0 :
         answer = answer[:-k]
     return ''.join(answer)
