79d1db795740bc5aa41695d1fcbda1f6cc0a2b24
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2025-04-24 18:38:34 +0900

    [level 3] Title: 단속카메라, Time: 1.18 ms, Memory: 9.75 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.py
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.py
@@ -1,11 +1,10 @@
 def solution(routes):
     answer = 1
     routes = list(sorted(routes,key = lambda x : x[1]))
-    r1, r2 = routes[0][0], routes[0][1]
+    now = routes[0][1]
     for i in range(1,len(routes)) :
         start, end = routes[i][0], routes[i][1]
-        if start > r2 :
+        if now < start :
+            now = end
             answer += 1
-            r2 = end
-        
     return answer
