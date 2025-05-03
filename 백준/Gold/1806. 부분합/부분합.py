64eb94ed1074d6b192294a70087a3c52fbc8dbca
Joo Yun <71087271+jooyun-1@users.noreply.github.com>
2025-05-03 22:38:12 +0900

    [Gold IV] Title: 부분합, Time: 108 ms, Memory: 42168 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,5 +1,7 @@
-N, S = map(int,input().split())
-arr = list(map(int,input().split()))
+import sys
+
+N, S = map(int, sys.stdin.readline().split())
+arr = list(map(int, sys.stdin.readline().split()))
 
 left, right = 0, 0
 answer = 1e9
